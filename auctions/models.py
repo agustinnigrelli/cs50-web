e5be23c66caefb4ccda2b2763e81708718cc19b2
agustinnigrelli <agustinnigrelli@gmail.com>
2020-10-18 22:49:51 -0300

    Added style to categories

--- auctions/models.py
+++ auctions/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
 
 class Categories(models.Model):
     category = models.CharField(max_length=50)
+    image = models.URLField(null=True)
 
     def __str__(self):
         return f"{self.category}"
