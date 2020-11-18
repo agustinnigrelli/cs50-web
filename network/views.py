f25e195f725d297dcd1257d277e078280205cece
agustinnigrelli <agustinnigrelli@gmail.com>
2020-11-18 16:48:22 -0300

    Added pagination function

--- network/views.py
+++ network/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.urls import reverse
 
 from .models import User, Followlist, Likelist , Posts, Comments
@@ -18,6 +19,12 @@ def index(request):
             ).order_by('-timestamp')
     else:
         posts = Posts.objects.all().order_by('-timestamp')
+
+    # Paginate the posts un groups of 10
+    page_number = request.GET.get('page')
+    
+    posts = Paginator(posts, 10)
+    posts = posts.get_page(page_number)
 
     return render(request, "network/index.html", {
         "posts": posts
@@ -116,6 +123,12 @@ def profile(request, user_id):
     else:
         posts = Posts.objects.filter(user=user_id).order_by('-timestamp')
 
+    # Paginate the posts un groups of 10
+    page_number = request.GET.get('page')
+    
+    posts = Paginator(posts, 10)
+    posts = posts.get_page(page_number)
+    
     # By default assume the target is not being followed
     not_following = "False"
     
@@ -155,11 +168,22 @@ def follow(request):
 @login_required
 def following(request):
     
-    # http://thecodelearners.com/django-quersets-one-to-one-foreign-key-inner-joins-query-filtering/
+    # Create a list of followed users by the current user
+    followlist = Followlist.objects.filter(user=request.user)
+    following = []
+    for user in followlist:
+        following.append(user.following)
 
-    posts = Posts.objects.filter().annotate(
+    # Filter the post by the users present in the list
+    posts = Posts.objects.filter(user__in=following).annotate(
         is_liked=Exists(Likelist.objects.filter(likes=OuterRef('pk'), user=request.user))
     ).order_by('-timestamp')
+
+    # Paginate the posts un groups of 10
+    page_number = request.GET.get('page')
+    
+    posts = Paginator(posts, 10)
+    posts = posts.get_page(page_number)
 
     return render(request, "network/following.html", {
         "posts": posts
@@ -176,7 +200,6 @@ def like(request):
         # If the post is not in user's likelist. Create a row and add 1 to the count
         if not Likelist.objects.filter(user=user, likes=post):
             
-            # PROBAR PASAR EL METODO A MANY TO MANY. O VER COMO LOGRAR QUE HAGA LA COMPARACION EN HTML
             likelist = Likelist(user=user, likes=post)
             likelist.save()
             post.likecount += 1
