12ba8fca2063b2ad6235e52cdd9ba9addf0927a6
agustinnigrelli <agustinnigrelli@gmail.com>
2020-11-20 14:56:02 -0300

    Added layout, register and login templates

---
+++ odeon/static/odeon/alerts.js
@@ -0,0 +1,107 @@
+/*global swal*/
+
+function validate_login() {
+  var username = document.forms["login"]["username"].value;
+  var password = document.forms["login"]["password"].value;
+  if (username == "") {
+    swal({
+        title: "Who are you?",
+        text: "It seems that you forgot to say your name"
+        });
+    return false;
+  } else if (password ==""){
+      swal({
+          title: "Not so fast!",
+          text: "It seems that you forgot to put your password"
+          });
+      return false;
+  }
+}
+
+function validate_account() {
+  var fname = document.forms["account"]["fname"].value;
+  var lname = document.forms["account"]["lname"].value;
+  var city = document.forms["account"]["city"].value;
+  var state = document.forms["account"]["state"].value;
+  if (fname == "" || lname == "" || city == "" || state =="") {
+    swal({
+        title: "Ups!",
+        text: "You must not leave any blank fields"
+        });
+    return false;
+  }
+}
+
+function validate_password() {
+  var password = document.forms["password"]["password"].value;
+  var newpassword = document.forms["password"]["newpassword"].value;
+  var newpasswordcon = document.forms["password"]["newpasswordcon"].value;
+  if (password == "") {
+    swal({
+        title: "Not so fast!",
+        text: "Ir order to change your password, you must first provide it"
+        });
+    return false;
+  } else if (newpassword =="" || newpasswordcon == ""){
+      swal({
+          title: "Pending confirmation",
+          text: "You must provide the new password twice"
+          });
+      return false;
+  }
+}
+
+function validate_register() {
+  var regusername = document.forms["register"]["regusername"].value;
+  var regemail = document.forms["register"]["regemail"].value;
+  var regpassword = document.forms["register"]["regpassword"].value;
+  var regpasswordcon = document.forms["register"]["regpasswordcon"].value;
+  if (regusername == "") {
+    swal({
+        title: "Ups!",
+        text: "You didn't put an username"
+        });
+    return false;
+  } else if (regemail ==""){
+      swal({
+          title: "Ups!",
+          text: "You mus provide an e-mail"
+          });
+      return false;
+  } else if (regpassword == "" || regpasswordcon == ""){
+      swal({
+          title: "Ups!",
+          text: "Password and/or confirmation pending"
+          });
+          return false;
+  }
+}
+
+function validate_tutor() {
+  var title = document.forms["tutor"]["title"].value;
+  var texttutor = document.forms["tutor"]["texttutor"].value;
+  var price = document.forms["tutor"]["price"].value;
+  var contact1 = document.forms["tutor"]["contact1"].value;
+  var contact2 = document.forms["tutor"]["contact2"].value;
+  if(title == "" || texttutor == "" || price == "" || contact1 == "" || contact2 == ""){
+    swal({
+      title: "Complete everything!",
+      text: "Every field is important. Please don't leavy any blank field"
+    });
+    return false;
+  }
+}
+
+function validate_student() {
+  var subject = document.forms["student"]["subject"].value;
+  var topic = document.forms["student"]["topic"].value;
+  var textstudent = document.forms["student"]["textstudent"].value;
+  var contact = document.forms["student"]["contact"].value;
+  if(subject == "" || topic == "" || textstudent == "" || contact == ""){
+    swal({
+      title: "Complete everything!",
+      text: "Every field is important. Please don't leavy any blank field"
+    });
+    return false;
+  }
+}
--- odeon/views.py
+++ odeon/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import datetime
 
-# Create your views here.
+@login_required
+def index(request):
+    return render(request, "odeon/index.html", {
+        "announcements": Announcement.objects.all()
+    })
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "odeon/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "odeon/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "odeon/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "odeon/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "odeon/register.html")
